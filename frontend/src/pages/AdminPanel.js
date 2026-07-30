1d102f6aae8ed3e457e8ca16a89fc83588f11312
muradmahmood46-star <muradmahmood46@gmail.com>
2026-07-30 16:40:44 +0500

    Fix build: remove BOM and add cross-env for CI

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import API from '../api';
 import { useNavigate } from 'react-router-dom';
 import '../panel.css';
